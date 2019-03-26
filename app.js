99e0c328ad1df8c22f492282e510a9cf3e33434b
unknown <hspreet1998@gmail.com>
2019-03-26 21:08:13 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load  Model
+require('./models/Idea');
+
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log(err));
+
+
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// Static folder
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+// Method override middleware
+app.use(methodOverride('_method'));
+
+// Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Index Route
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        title: 'Welcome'
+    });
+});
+
+// About Route
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
